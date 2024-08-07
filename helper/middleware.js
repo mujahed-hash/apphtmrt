17c25b2057e25a5b6bb0d682562ce123524edeee
mujahed-hash <mujahedahmed786@gmail.com>
2024-08-07 06:54:23 -0600

    app v1-4c

--- helper/middleware.js
+++ helper/middleware.js
@@ -1,4 +1,4 @@
-const jwt2 = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 // const authorize = jwt({
 //     secret: process.env.secret,
 //     userProperty: 'payload',
@@ -9,20 +9,22 @@ const jwt2 = require('jsonwebtoken');
 //     authorize
 // }
 
-
-module.exports.verifyToken =  (req, res, next) => {
-  
+module.exports.verifyToken = async (req, res, next) => {
     const token = req.header('Authorization')?.replace('Bearer ', '');
-    if (!token) return res.status(401).send('Access denied. No token provided.');
-
-    try {
-        const decoded = jwt2.verify(token, process.env.SECRET);
-        req.user = decoded; // Attach the decoded token information to req.user
-        req.userId = decoded.userId
-        // req.user = await User.findById(decoded._id).select('-password');
-        next();
-    } catch (ex) {
-        res.status(400).send('Invalid token.');
+    if (!token) {
+        console.error('No token provided');
+        return res.status(401).send('Access denied. No token provided.');
     }
 
+    try {
+        console.log('Token received:', token);
+        const decoded = jwt.verify(token, process.env.SECRET);
+        console.log('Token decoded:', decoded);
+        req.user = decoded;
+        req.userId = decoded.userId;
+        next();
+    } catch (ex) {
+        console.error('Token verification failed:', ex.message);
+        res.status(400).send('Invalid token.');
+    }
 };
