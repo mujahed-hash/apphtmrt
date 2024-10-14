074abc384c6c1e098e430afcc96325ceb4db2f66
mujahed-hash <mujahedahmed786@gmail.com>
2024-10-14 16:52:12 -0500

    update many

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var path = require('path');
 
 app.use(cors());
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '80mb' }));
+app.use(express.urlencoded({ limit: '80mb', extended: true }));
 
 
 app.use('/uploads/products', express.static(__dirname + '/uploads/products'))
